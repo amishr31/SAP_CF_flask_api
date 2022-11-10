1c8652e9e15bf5b969aba613796277ea8027d1a7
amishr31 <117950140+amishr31@users.noreply.github.com>
2022-11-11 03:26:47 +0530

    Add files via upload

---
+++ python-deploy/server.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 3000))
+@app.route('/')
+def hello():
+    return "Nice Try! Here's Hello World for you...see you again"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
